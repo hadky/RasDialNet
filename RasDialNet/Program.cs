823773dc657a656264073b9e97db933317e35fbe
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2014-07-21 17:06:25 +0100

    Initial commit using DotRas

---
+++ RasDialNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotRas;
+
+namespace RasDialNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dialer = new RasDialer();
+            dialer.AllowUseStoredCredentials = true;
+            dialer.EntryName = args[0];
+            dialer.PhoneBookPath = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User);
+            var handle = dialer.Dial();
+        }
+    }
+}
