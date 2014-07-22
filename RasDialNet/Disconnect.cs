b282ee102d72dfb4323e8b20f98fe5479579703b
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2014-07-23 00:13:56 +0100

    Impl'd disconnection.

---
+++ RasDialNet/Disconnect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DotRas;
+
+namespace RasDialNet
+{
+    class Disconnect : Command
+    {
+        private readonly RasDialOptions _options;
+
+        public Disconnect(RasDialOptions options)
+        {
+            if (options == null) throw new ArgumentNullException("options");
+            _options = options;
+        }
+
+        public override void Execute()
+        {
+            var connections = RasConnection.GetActiveConnections();
+            foreach (var connection in connections)
+            {
+                if (connection.EntryName.Equals(_options.ConnectionName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    connection.HangUp(true);
+                }
+            }
+        }
+    }
+}
